8993b8b09a3f7acdac37fdc36a54380000d87967
nautomations-in <nautomations.in@gmail.com>
2026-09-25 17:08:13 +0530

    Jump to #section instantly on arrival

--- js/script.js
+++ js/script.js
@@ -170,7 +170,7 @@ window.addEventListener('load', () => {
     const target = document.querySelector(location.hash);
     if (target) {
       const top = target.getBoundingClientRect().top + window.pageYOffset - 84;
-      window.scrollTo({ top, behavior: 'smooth' });
+      window.scrollTo(0, top);
     }
   }
 });
